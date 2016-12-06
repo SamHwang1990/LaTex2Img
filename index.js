3663728daa894e550e66d26de8ed81384f2995a2
SamHwang1990 <samhwang1990@gmail.com>
2016-12-06 08:56:14 +0800

    add png support

--- index.js
+++ index.js
@@ -59,39 +59,81 @@ app.use(function* visitConverterPage(next) {
 });
 
 app.use(function* covertResult(next) {
-  this.body = yield next;
+  ({type: this.response.type, result: this.body} = yield next);
 });
 
 // TODO: POST 的请求应该时怎样的情境?
 app.use(function* handleGet(next) {
   var mathContent;
+  var convertType;
   if (this.method === 'POST') {
-    mathContent = this.request.body.content;
+    ({content: mathContent, type: convertType} = this.request.body);
   }
 
   if (!mathContent) {
     mathContent = this.query.eq ? decodeURIComponent(this.query.eq) : '';
   }
+  if (!convertType) {
+    convertType = this.query.type ? decodeURIComponent(this.query.type) : '';
+  }
 
-  logger.info(`Received math content: ${mathContent}`);
+  logger.info(`Received math content: ${mathContent} and convert type: ${convertType}`);
 
   if (!mathContent) return '';
-  return doConvert(mathContent);
+  if (!convertType) convertType = 'png';
+  return doConvert(mathContent, convertType);
 });
 
 // TODO: 支持多类型的转换
-function doConvert(content) {
+function doConvert(content, type) {
+  function initMjData(content, type) {
+    type = type ? type.toLowerCase() : '';
+
+    let mjData = {
+      math: content,
+      format: 'TeX'
+    };
+
+    if (type === 'svg') {
+      mjData.svg = true;
+      return mjData;
+    }
+
+    if (type === 'png') {
+      mjData.png = true;
+      mjData.width = 100;
+      mjData.ex = 6;
+      mjData.dpi = mjData.ex * 16;
+      mjData.linebreaks = true;
+      return mjData;
+    }
+  }
+
+  function outputResult(type, data) {
+    type = type ? type.toLowerCase() : '';
+
+    if (type === 'svg') {
+      return {
+        type: 'image/svg+xml',
+        result: data.svg
+      }
+    }
+
+    if (type === 'png') {
+      return {
+        type: 'image/png',
+        result: new Buffer(data.png.slice(22),'base64')
+      }
+    }
+  }
+
   return new Promise((resolve, reject) => {
     mjApi.config({});
     mjApi.start();
 
-    mjApi.typeset({
-      math: content,
-      format: "TeX",
-      svg: true
-    }, function (data) {
+    mjApi.typeset(initMjData(content, type), function (data) {
       if (data.errors) return reject(data.errors);
-      resolve(data.svg);
+      resolve(outputResult(type, data));
     });
   });
 }
