d0bdcc8aff5924154f41da1cadf2ca634f82a238
SamHwang1990 <samhwang1990@gmail.com>
2016-12-08 22:36:27 +0800

    do not support png

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
-mkdir ./tmp;
-curl "http://apache.01link.hk/xmlgraphics/batik/binaries/batik-1.7.1.tar.gz" -o "./tmp/batik-1.7.1.tar.gz";
-npm install;
-tar -xzf ./tmp/batik-1.7.1.tar.gz -C ./tmp/;
-cp ./tmp/batik-1.7.1/batik-rasterizer.jar ./node_modules/MathJax-node/batik/;
-cp -R ./tmp/batik-1.7.1/lib ./node_modules/MathJax-node/batik/;
-rm -rf ./tmp/;
+# no need to support png now
+#mkdir ./tmp;
+#curl "http://apache.01link.hk/xmlgraphics/batik/binaries/batik-1.7.1.tar.gz" -o "./tmp/batik-1.7.1.tar.gz";
+#npm install;
+#tar -xzf ./tmp/batik-1.7.1.tar.gz -C ./tmp/;
+#cp ./tmp/batik-1.7.1/batik-rasterizer.jar ./node_modules/MathJax-node/batik/;
+#cp -R ./tmp/batik-1.7.1/lib ./node_modules/MathJax-node/batik/;
+#rm -rf ./tmp/;
 
 exit 0;
 
